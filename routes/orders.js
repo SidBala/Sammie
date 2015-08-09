5fcef63a79c8c90fb91ec7ad4ea5fda084a35c05
Johnny Ting Shi <johnny.t.shi@gmail.com>
2015-08-09 16:47:32 -0700

    use supervisor to auto restart app

--- routes/orders.js
+++ routes/orders.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.post('/new', function(req, res, next) {
-    res.send('Ordering for ' + req.body.person);
+    res.send('Ordering for you: ' + req.body.person);
 });
 
 module.exports = router;
